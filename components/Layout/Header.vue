02a0961c94d2ec14d614c15d14e19447aea83fc3
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2023-04-15 13:42:33 +0300

    Add layout

---
+++ components/Layout/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header><nav>Mr Beam</nav></header>
+</template>
+
+<style lang="scss">
+header {
+  background-color: $red;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">
+    <LayoutHeader />
+    <main>
+      <NuxtPage />
+    </main>
+  </div>
+</template>
